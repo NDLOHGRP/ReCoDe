6ea609867ba4d7f5e71eec7bcdcec8a3423cdfce
abhikdatta <findabhik@gmail.com>
2018-09-12 12:44:58 +0800

    Added external dependencies

---
+++ lib/snappy-master/build/CMakeFiles/snappy_unittest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snappy_unittest.dir/snappy_unittest.cc.o"
+  "CMakeFiles/snappy_unittest.dir/snappy-test.cc.o"
+  "snappy_unittest.pdb"
+  "snappy_unittest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snappy_unittest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lib/snappy-master/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/abhik/code/DataCompression/c/compressors/snappy-master
+# Build directory: /home/abhik/code/DataCompression/c/compressors/snappy-master/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(snappy_unittest "/home/abhik/code/DataCompression/c/compressors/snappy-master/build/snappy_unittest")
+set_tests_properties(snappy_unittest PROPERTIES  WORKING_DIRECTORY "/home/abhik/code/DataCompression/c/compressors/snappy-master")
